39a6b6ca286595dcf88259c1ab7e708e4a7a2991
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-09 09:31:11 -0600

    updated

--- src/Vowels.java
+++ src/Vowels.java
@@ -1,8 +1,14 @@
 public class Vowels {
     public static int getCount(String str) {
-        int vowelsCount = 0;
 
-        return vowelsCount;
+        int vowelsCount = 0;
+        for(int i = 0; i < str.length(); i++) {
+            if (str.toLowerCase().contains()) {
+                vowelsCount++;
+            }
+        }
+
+            return vowelsCount;
     }
 
 
