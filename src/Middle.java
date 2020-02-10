05e476f2e2d3daf1af389b7747fb4887f075f2e8
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-09 18:56:33 -0600

    updated

--- src/Middle.java
+++ src/Middle.java
@@ -1,7 +1,10 @@
 public class Middle {
     public static String getMiddle(String word) {
+        String middle;
         for(int i = 0; i < word.length(); i++) {
-            if(word)
+            if(word.length() % 2 == 1) {
+
+            }
 
         }
     }
