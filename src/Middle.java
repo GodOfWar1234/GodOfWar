97260326958e5aa8f591fb901ce5906259414f7a
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-10 07:14:46 -0600

    updated

--- src/Middle.java
+++ src/Middle.java
@@ -1,13 +1,15 @@
 public class Middle {
     public static String getMiddle(String word) {
-            int length = word.length();
-        int middle = length / 2;
-        if(length % 2 != 0) {
-               word.substring(0, middle);
-            } else {
-
+        StringBuilder sb = new StringBuilder(word);
+        if(word.length() % 2 != 0)
+            sb.deleteCharAt(word.length()/2);
+        else{
+            int middle = word.length()/2 - 1;
+            sb.deleteCharAt(middle);
+            sb.deleteCharAt(middle);
         }
-
+        word = sb.toString();
+        return word;
     }
 
 }
