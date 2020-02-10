0847363d7fa71aa8a067a0b88ef495eda9804342
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-10 11:00:02 -0600

    updated

--- src/Accumul.java
+++ src/Accumul.java
@@ -1,5 +1,9 @@
 public class Accumul {
     public static String accum(String s) {
+       String upperCase = s.substring(0, 1).toUpperCase() + s.substring(1);
+    for(int i = 0; i < s.length(); i++) {
+
+    }
 
     }
 
