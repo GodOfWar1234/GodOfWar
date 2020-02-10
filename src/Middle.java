fe52f53a4803017e3dfd368887857a27ef8ff627
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-09 18:44:46 -0600

    updated

---
+++ src/Middle.java
@@ -0,0 +1,9 @@
+public class Middle {
+    public static String getMiddle(String word) {
+        for(int i = 0; i < word.length(); i++) {
+            if(word)
+
+        }
+    }
+
+}
--- src/Vowels.java
+++ src/Vowels.java
@@ -3,7 +3,8 @@ public class Vowels {
 
         int vowelsCount = 0;
         for(int i = 0; i < str.length(); i++) {
-            if (str.toLowerCase().contains()) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' ||
+                    str.charAt(i) == 'o' || str.charAt(i) == 'u') {
                 vowelsCount++;
             }
         }
