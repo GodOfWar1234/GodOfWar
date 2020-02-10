4df07a596a811a4ebd8123c81efb49d391511862
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-10 10:09:28 -0600

    updated

--- src/Middle.java
+++ src/Middle.java
@@ -2,9 +2,10 @@ public class Middle {
     public static String getMiddle(String word) {
         String answer;
         int middle = word.length() / 2;
+        int length;
         if(word.length() % 2 == 0) {
             answer = word.substring(0, middle);
-
+            length = 2;
        } else {
      answer = word.substring(0, 1);
        }
