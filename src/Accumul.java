3465e6e4c089250dcc01320715aae36663704fd9
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-16 19:06:07 -0600

    updated

--- src/Accumul.java
+++ src/Accumul.java
@@ -1,14 +1,17 @@
 public class Accumul {
-    public static String accum(String s) {
-       String upperCase = s.substring(0, 1).toUpperCase() + s.substring(1);
-    for(int i = 0; i < s.length(); i++) {
 
+
+    public static String accum(String s) {
+    for(int i = 0; i < s.length(); i++) {
+       String upperCase = s.substring(0, 1).toUpperCase() + s.substring(1);
+       upperCase.replace("", " ").trim();
     }
 
+    return
     }
 
     public static void main(String[] args) {
-        System.out.println(accum("based");
+        System.out.println(accum("based"));
     }
 
 
